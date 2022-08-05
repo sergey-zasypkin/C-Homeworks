bbbc394058055bf4f5b8d65a59aa1ea8ab735c6c
Sergey Z <zassipkin.com>
2022-08-05 14:14:09 +0300

    Task8

---
+++ HW_1/Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// N - простое число.
+
+Console.Write("Type the number:");
+string S1 = Console.ReadLine();
+int numberA = int.Parse(S1);
+
+int numberI = 1;
+
+while(numberI <= numberA)
+{
+    if(numberI % 2 == 0)
+    {
+        Console.Write(numberI + " ");
+        numberI = numberI + 1;
+    }
+    else
+    {
+        numberI = numberI + 1;
+    }
+}
