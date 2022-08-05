a89fe1d560613e79257d49264453afea5bfe4fde
Sergey Z <zassipkin.com>
2022-08-05 13:35:48 +0300

    Task6

---
+++ HW_1/Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Подразумеваем простые числа
+
+Console.Write("Type the first number:");
+string S1 = Console.ReadLine();
+int numberA = int.Parse(S1);
+
+if (numberA % 2 == 1)
+{
+    Console.WriteLine("uneven");
+}
+else
+{
+    Console.WriteLine("even");
+}
