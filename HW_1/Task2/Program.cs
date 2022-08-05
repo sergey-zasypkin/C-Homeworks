2a903022a3cb962933c7f7b37af05fe26817f6ad
Sergey Z <zassipkin.com>
2022-08-05 12:27:38 +0300

    Task2 upd

--- HW_1/Task2/Program.cs
+++ HW_1/Task2/Program.cs
@@ -4,6 +4,7 @@
 Console.Write("Type the first number:");
 string S1 = Console.ReadLine();
 int numberA = int.Parse(S1);
+
 Console.Write("Type the second number:");
 string S2 = Console.ReadLine();
 int numberB = int.Parse(S2);
